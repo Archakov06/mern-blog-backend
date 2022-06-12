281996444aad3dc90e231ece70d4a156a5dd46fd
Archakov Dennis <amonbower@gmail.com>
2022-06-13 02:39:12 +0300

    fix index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -57,7 +57,7 @@ app.patch(
   PostController.update,
 );
 
-app.listen(process.env.PORT || 3000, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
